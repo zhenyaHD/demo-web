7eb0d548a35cbe1d388a306f4a315b1533fd4ebc
Zhenya <zhenya951236qw@mail.ru>
2023-02-08 18:05:35 +0500

    Отображение поста

--- src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -1,6 +1,7 @@
 package com.example.demoweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,5 +12,11 @@ public class PostsViewController {
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String list() {
         return "Здесь будет главная страница";
+    }
+
+    @ResponseBody
+    @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
+    public String single(@PathVariable("id") Long id) {
+        return "Здесь будет страница поста №" + id;
     }
 }
