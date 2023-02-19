e116e518a540f5ae02c8d7e28ea6a89b038762d3
Zhenya <zhenya951236qw@mail.ru>
2023-02-19 19:56:31 +0500

    Добовление поста

---
+++ src/main/java/com/example/demoweb/controller/PostsCreateController.java
@@ -0,0 +1,27 @@
+package com.example.demoweb.controller;
+
+import com.example.demoweb.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PostsCreateController {
+    @Autowired
+    PostService postsService;
+
+    @RequestMapping(path = "/new", method = RequestMethod.GET)
+    public String create(Model model) {
+        model.addAttribute("appName", "Моё супер приложение");
+        return "create";
+    }
+
+    @RequestMapping(path = "/new", method = RequestMethod.POST)
+    public String doCreate(@ModelAttribute("text") String text) {
+        postsService.create(text);
+        return "redirect:/";
+    }
+}
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -7,13 +7,17 @@ import java.util.*;
 
 @Service
 public class PostService {
+    private List<Post> posts = new ArrayList<>() {{
+        add(new Post("Погиб знаменитый актёр Руслан Кравченко", new Date()));
+        add(new Post("В Китае назначили нового министра иностранных дел", new Date()));
+        add(new Post("В результате обрушения здания в Египте погибло 48 человек", new Date()));
+    }};
+
     public List<Post> listAllPosts() {
-        List<Post> posts = new ArrayList<Post>();
-
-        posts.add(new Post("Погиб знаменитый актёр Руслан Кравченко", new Date()));
-        posts.add(new Post("В Китае назначили нового министра иностранных дел", new Date()));
-        posts.add(new Post("В результате обрушения здания в Египте погибло 48 человек", new Date()));
-
         return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
